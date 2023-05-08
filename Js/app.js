a30443042c65b07fcabb4cea5b05c362ea49a27d
Pablo Moore <105167808+PabloMoore96@users.noreply.github.com>
2023-05-08 02:58:09 -0300

    Últimos cambios hechos por hoy
    Todavía no termino el carroussel y obviamente no incorporé js al html.

---
+++ Js/app.js
@@ -0,0 +1,16 @@
+const btnRight = document.querySelector("#btnRight")
+const btnLeft = document.querySelector("#btnLeft")
+
+const img = document.querySelector("")
+
+// /* necesito que al presionar el boton right
+// la imagen de nombre Banner1-carroussel,Lm.webp cambie a
+// Banner2-carroussel,Lm.webp y así hasta el 5
+// */
+
+// let arrayImg = ["./Assets/Img/Banner1-carroussel,Lm.webp", "./Assets/Img/Banner2-carroussel,Lm.webp","./Assets/Img/Banner3-carroussel,Lm.webp","./Assets/Img/Banner4-carroussel,Lm.webp","./Assets/Img/Banner5-carroussel,Lm.webp"]
+
+btnRight.addEventListener('click', () =>{
+    
+
+});
