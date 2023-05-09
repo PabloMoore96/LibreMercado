bdd198064491d5bc7aa1e20fc62138270a79d54c
Pablo Moore <105167808+PabloMoore96@users.noreply.github.com>
2023-05-09 03:35:43 -0300

    Mejora diseño carroussel y funcionalidad
    Ahora el archivo de JS si está integrado a la página, y el carroussel ya tiene funcionalidad.

--- Js/app.js
+++ Js/app.js
@@ -1,16 +1,38 @@
-const btnRight = document.querySelector("#btnRight")
-const btnLeft = document.querySelector("#btnLeft")
 
-const img = document.querySelector("")
 
-// /* necesito que al presionar el boton right
-// la imagen de nombre Banner1-carroussel,Lm.webp cambie a
-// Banner2-carroussel,Lm.webp y así hasta el 5
-// */
+const arrayImg = [
+    "./Assets/Img/Banner1-carroussel,Lm.webp",
+    "./Assets/Img/Banner2-carroussel,Lm.webp",
+    "./Assets/Img/Banner3-carroussel,Lm.webp",
+    "./Assets/Img/Banner4-carroussel,Lm.webp",
+    "./Assets/Img/Banner5-carroussel,Lm.webp"
+];
 
-// let arrayImg = ["./Assets/Img/Banner1-carroussel,Lm.webp", "./Assets/Img/Banner2-carroussel,Lm.webp","./Assets/Img/Banner3-carroussel,Lm.webp","./Assets/Img/Banner4-carroussel,Lm.webp","./Assets/Img/Banner5-carroussel,Lm.webp"]
+document.Imagen.src = arrayImg[0];
 
-btnRight.addEventListener('click', () =>{
+const rightImg = document.querySelector(".caright");
+const leftImg = document.querySelector(".caleft");
+let cont = 0
+
+function mover_derecha(){
+    cont++;
+    if (cont > 4){
+        cont = 0
+    }
+    console.log(cont)
+    document.Imagen.src = arrayImg[cont];
     
 
-});
+}
+function mover_izquierda(){
+    cont--;
+    if (cont < 0){
+        cont = 4
+    }
+    console.log(cont)
+    document.Imagen.src = arrayImg[cont];
+    
+}
+
+rightImg.addEventListener("click", mover_derecha);
+leftImg.addEventListener("click", mover_izquierda);
